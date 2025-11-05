421238b9c822d198c50abba03ccf611f6e5523f8
Amna Mahmood <amahmood@gmail.com>
2025-11-05 13:49:38 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print ("Hello")
+student_name: str = "Ahmad Jamal Khan"
+student_age: int =17
+student_grade: str = "11th" 
+student_gpa : float=3.9
+print (f"Student Name = {student_name}\nAge = {student_age}\nGrade = {student_grade}\nGPA = {student_gpa}")
